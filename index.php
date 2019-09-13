01e23292356b2692b81553f8eed2041f324a08e8
millieb <mildred.brito@outlook.com>
2019-09-13 17:55:07 -0600

    Adding PHP to an HTML Website

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
-<?php 
-echo 'Hola ', 'mundo ', 'adios ',1;
-print 'Hola mundo';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>    
+    </head>
+    <body>
+        <table>
+            <?php 
+            $array=array('name'=>'Name', 'lastName'=>'Last Name', 'address'=>'Address', 'phone'=>'Phone');
+            foreach($array as $key=>$value):
 
-$animal = "Perrito";
-echo "Hola $animal\n adios";
+            ?>
+            <tr>
+                <td><?php echo $value?>
+                </td>
+                <td>
+                   <input type="text" name=<?php echo $key?>> 
+                </td>
+            </tr>
+            <?php endforeach; ?> 
+            
 
-?>
+            <tr>
+                <td>Sex
+                </td>
+                <td>
+                   <input type="radio" name='sex' value='Mujer'>Mujer
+                   <input type="radio" name='sex' value='Hombre'>Hombre
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="Guardar">
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
 
